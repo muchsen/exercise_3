66cf226724118993b4ef511bbe748495c69c87c3
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-11-21 16:06:49 +0700

    add public bool search for the specified node

--- exercise_3/Program.cs
+++ exercise_3/Program.cs
@@ -20,6 +20,19 @@ namespace Exercise_linked_list_A
         {
             LAST = null;
         }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);
+            }
+            if (rollNo == LAST.rollNumber)
+                return true;
+            else
+                return false;
+        }
         
     }
     
