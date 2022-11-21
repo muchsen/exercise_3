52653452566c7527c58d21b9f72de234a85379aa
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-11-21 15:45:49 +0700

    add clas node

--- exercise_3/Program.cs
+++ exercise_3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Exercise_linked_list_A
+{
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public string next;
+    }
+   
+}
